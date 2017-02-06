9d08b81e1b617b928778d9500e60d5cbca45033a
Logan Franken <loganfranken@gmail.com>
2017-02-05 17:39:00 -0800

    Completed Piece A

---
+++ A/script/main.js
@@ -0,0 +1,170 @@
+'use strict';
+
+const maxFrames = 20;
+let frames = [];
+
+const loadFrame = (index) => {
+
+  let image = new Image();
+  image.src = `images/${index}.jpg`;
+  image.onload = () => {
+
+    let canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    canvas.getContext('2d').drawImage(image, 0, 0);
+
+    onLoadedFrame(canvas.toDataURL(), index);
+
+  };
+
+}
+
+const onLoadedFrame = (image, index) => {
+
+  frames[index] = image;
+
+  if(frames.length >= maxFrames)
+  {
+    onAllFramesLoaded();
+  }
+
+}
+
+const onAllFramesLoaded = () => { start() }
+
+const start = () => {
+
+  // Randomize the frames (except for the first one)
+  shuffle(frames);
+
+  let sortedFrames = [];
+
+  let lastFrame = frames[0];
+  sortedFrames.push(lastFrame);
+  frames[0] = null;
+
+  // Play the initial frame
+  playSortedFrames(sortedFrames);
+
+  const advanceSort = () => {
+
+    // Calculate how similar this frame is to all of the other frames
+    let mostSimilarFrame = null;
+
+    let diffCalcPromises = frames.map((frame, index) => {
+      return new Promise((resolve, reject) => {
+
+        if(frame === null)
+        {
+          resolve();
+          return;
+        }
+
+        resemble(lastFrame).compareTo(frame).ignoreColors().onComplete((data) => {
+
+          let similarity = (100 - data.misMatchPercentage);
+
+          if(mostSimilarFrame === null || similarity > mostSimilarFrame.similarity)
+          {
+            mostSimilarFrame = {
+              similarity,
+              index
+            };
+          }
+
+          resolve();
+
+        });
+
+      });
+    });
+
+    // Set the most similar frame as the next frame
+    Promise.all(diffCalcPromises).then(() => {
+
+      let lastFrame = frames[mostSimilarFrame.index];
+      sortedFrames.push(lastFrame);
+      frames[mostSimilarFrame.index] = null;
+
+    });
+
+    // Play the current movie and continue the loop
+    playSortedFrames(sortedFrames).then(() => {
+
+      if(sortedFrames.length >= frames.length)
+      {
+        // Reset and restart
+        frames = sortedFrames.map((frame) => (frame));
+        sortedFrames = [];
+        start();
+      }
+      else
+      {
+        advanceSort();
+      }
+
+    });
+
+  }
+
+  advanceSort();
+
+}
+
+const playSortedFrames = (sortedFrames) => {
+  return new Promise((resolve, reject) => {
+
+    let currIndex = 0;
+
+    const nextFrame = () => {
+
+      if(currIndex >= sortedFrames.length)
+      {
+        resolve();
+        return;
+      }
+
+      document.getElementById('screen').setAttribute('src', sortedFrames[currIndex++]);
+      setTimeout(nextFrame, 100);
+
+    };
+
+    nextFrame();
+
+  });
+};
+
+// Source: http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+const shuffle = (array) => {
+
+  let currentIndex = array.length;
+  let temporaryValue = null;
+  let randomIndex = null;
+
+  // Leave the first frame in place
+  while (1 !== currentIndex)
+  {
+    do
+    {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+    }
+    while(randomIndex === 0)
+
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+
+}
+
+// Load all images
+for(let i=0; i<maxFrames; i++)
+{
+  loadFrame(i);
+}
